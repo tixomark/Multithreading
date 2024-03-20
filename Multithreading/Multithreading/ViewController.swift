73039d91acb351801a2d52a97d10a72b6578a1fc
tixomark <tixomark@mail.ru>
2024-03-20 14:45:21 +0300

    initial commit

---
+++ Multithreading/Multithreading/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreading
+//
+//  Created by Tixon Markin on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
